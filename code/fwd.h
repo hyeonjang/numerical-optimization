b3ac0bac336384bec9d9ad24203b12fb6b6145ea
hyeonjang <ippguu@gmail.com>
2021-09-29 19:41:41 +0900

    Add include guard

--- code/fwd.h
+++ code/fwd.h
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef __FWD_H__
+#define __FWD_H__
+
 #include <functional>
 #include <limits>
 #include <vector>
@@ -7,15 +11,20 @@ namespace numerical_optimization {
 namespace uni {
     using function_t = std::function<float(const float&)>;
     using boundary_t = std::pair<float, float>;
-}
+};
 
 namespace multi {
-    using function_t = std::function<std::vector<float>(const std::vector<float>&)>;
-}
+    using function_t = std::function<float(const std::vector<float>&)>;
+};
 
 constexpr float  MIN = 1e-4;// std::numeric_limits<float>::min();
 constexpr float  MAX = std::numeric_limits<float>::max();
 constexpr float  GOLDEN_RATIO = 1.f/1.618033988749895f;
 constexpr size_t FIBONACCI_MAX = 46;
 
+class Univariate;
+class Multivariate;
+
 }
+
+#endif // __FWD_H__
