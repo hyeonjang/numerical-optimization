ae07fccfacf14aefbeb4b76f9684b7e42b58f27a
hyeonjang <ippguu@gmail.com>
2021-10-11 21:00:08 +0900

    Add benchmark

--- test/hw3/benchmark.cpp
+++ test/hw3/benchmark.cpp
@@ -3,9 +3,31 @@
 #include <functional>
 #include <benchmark/benchmark.h>
 
+#include "multi/powells.hpp"
+#include "multi/nelder_mead.hpp"
 #include "../function.hpp"
 
 using namespace numerical_optimization;
 using namespace numerical_optimization::multi;
+
+std::vector<function_t<Vector2f>> functions = construct_functions();
+auto neldermead = construct_methods<NelderMead<Vector2f>>(functions);
+auto powells    = construct_methods<Powells<Vector2f>>(functions);
+
+static void bench_neldermead(benchmark::State& state) {
+    for(auto _ : state) {
+        neldermead[state.range(0)].eval();
+    }
+}
+
+static void bench_powells(benchmark::State& state) {
+    for(auto _ : state) {
+        powells[state.range(0)].eval();
+    }
+}
+
+
+BENCHMARK(bench_neldermead)->Arg(0)->Arg(1)->Arg(2);
+BENCHMARK(bench_powells)->Arg(0)->Arg(1)->Arg(2);
 
 BENCHMARK_MAIN();
