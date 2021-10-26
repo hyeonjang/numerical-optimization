d29d8cf23c4e1bb7c83472bac7947efc253734a7
hyeonjang <ippguu@gmail.com>
2021-10-26 23:41:24 +0900

    Change to template random

--- code/method.h
+++ code/method.h
@@ -2,6 +2,8 @@
 #define __METHOD_H__
 
 #include <iostream>
+#include <random>
+#include <algorithm>
 
 #include "fwd.h"
 
@@ -9,7 +11,27 @@ namespace numerical_optimization {
 
 class Method {
 public:
-    int random_int() const;
+    Method():iter(max_iter){};
+    Method(size_t it):iter(it){};
+
+protected:
+    size_t iter;
+
+public: 
+    template<typename Type>
+    Type random() const {
+    // threshold
+    constexpr int scale = 100000;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(
+        std::numeric_limits<Type>::min()/scale, 
+        std::numeric_limits<Type>::max()/scale
+        );
+    return distrib(gen);
+}
+
 };
 
 /////////////////////////////////////////////////////
