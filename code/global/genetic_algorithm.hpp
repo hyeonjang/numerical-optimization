16b449d88ee8637a0a056930a6d91d979a8a6c4b
hyeonjang <ippguu@gmail.com>
2021-12-15 14:08:31 +0900

    Done impl:genetic algorithm

--- code/global/genetic_algorithm.hpp
+++ code/global/genetic_algorithm.hpp
@@ -12,11 +12,11 @@ static std::random_device rd;
 static std::mt19937 gen(rd());
 
 template<typename pheno_t, size_t size>
-struct chromosome {
+struct chromosome_t {
     using bit_t = std::bitset<size>;
 
-    chromosome(){
-
+    chromosome_t(){
+        // binary distribution
         std::bernoulli_distribution d(0.5);
 
         for(size_t i=0; i<size; i++) {
@@ -36,11 +36,15 @@ struct chromosome {
         gene.flip(index);
     }
 
-    void crossover(chromosome<pheno_t, size>& other, size_t end, size_t start=0) {
+    void crossover(chromosome_t<pheno_t, size>& other, size_t end, size_t start=0) {
+        
+        size_t min, max;
+        std::tie(min, max) = std::minmax(end, start);
+
         // one point & two points crossover
         bit_t t_this  = this->gene;
         bit_t t_other = other.gene;
-        for(size_t i=end; i>start; i--) {
+        for(size_t i=max; i>min; i--) {
             this->gene[i] = t_other[i];
             other.gene[i] = t_this[i];
         }
@@ -50,83 +54,181 @@ struct chromosome {
         return gene.to_string();
     }
 
-    std::bitset<size> gene;
+private:
+    bit_t gene;
 };
 
-template<typename vector_t, typename scalar_t = typename std::conditional<std::is_base_of<Eigen::EigenBase<vector_t>, vector_t>::value, vector_t, typename vector_t::Scalar>::type>
+template<typename vector_t, typename scalar_t, size_t size>
+struct population_t {
+
+    using function_t = std::function<scalar_t(const vector_t&)>;
+
+    population_t():chromosome(chromosome_t<vector_t, size>()),fitness(0),probability(0) {}
+
+    scalar_t eval_fitness(const function_t& function){
+        return fitness = function(chromosome.decode());
+    };
+
+    double eval_probability(const scalar_t& sum_fitness, const size_t& total_size) {
+        return probability = ((sum_fitness-fitness)/sum_fitness)/(total_size-1);
+    }
+
+    chromosome_t<vector_t, size> chromosome;
+    scalar_t fitness;
+    double   probability;
+};
+
+// = typename std::conditional<std::is_base_of<Eigen::EigenBase<vector_t>, vector_t>::value, vector_t, typename vector_t::Scalar>::type>
+template<size_t len, typename vector_t, typename scalar_t>
 class GeneticAlgorithm : Method {
 public:
     using boundary_t = std::pair<scalar_t, scalar_t>;
     using function_t = std::function<scalar_t(const vector_t&)>;
-    using population_t = std::pair<chromosome<vector_t, 8>, scalar_t>;
+    using populate_t = population_t<vector_t, scalar_t, len>;
 
-    GeneticAlgorithm(function_t func, size_t size):function(func),population(size) {
-        initialize_population();
-    }
-    GeneticAlgorithm(function_t func):function(func),population(20) {
+    GeneticAlgorithm(function_t func, size_t size=20, double crossover_prob=0.7, double mutation_prob=0.1)
+    :function(func),population(size),prob_crossover(crossover_prob),prob_mutation(mutation_prob),sum_fitness(0),sum_probability(0) {
         initialize_population();
         evaluate_fitness();
+    }
 
-    };
+    void run(size_t iteration) {
 
+        for(size_t i=0; i<iteration; i++) {
+            // selection
+            size_t idx_x, idx_y;
+            std::tie(idx_x, idx_y) = select_with_rouletewheeling();
+
+            /////////////////////////////////////////////////
+            // reproduction
+            /////////////////////////////////////////////////
+            populate_t x = population[idx_x];
+            populate_t y = population[idx_y];
+            // cross over
+            if(crossover(x, y)) {
+                x.eval_fitness(function); 
+                y.eval_fitness(function);
+            }
+
+            // mutation
+            if(mutation(x) || mutation(y)) {
+                x.eval_fitness(function); 
+                y.eval_fitness(function);
+            }
+
+            /////////////////////////////////////////////////
+            // replacement
+            /////////////////////////////////////////////////
+            // replacement worst
+            {
+                replace_worst(x);
+                replace_worst(y);
+            }
+
+            // update whole populations; lazy method
+            evaluate_fitness();
+            std::cout << sum_fitness << std::endl;
+        }
+    }
+
+    void print() {
+        for(auto& p:population) {
+            std::cout << p.chromosome.to_string() << std::endl;
+            std::cout << p.fitness << std::endl;
+            std::cout << p.probability << std::endl;
+        }
+    }
+
+private:
     void initialize_population() {
         // randomly initialize population from chromosome
         for(size_t i=0; i<population.size(); i++) {
-            population[i] = std::make_pair(chromosome<vector_t, 8>(), NULL);
+            population[i] = populate_t();
         }
     }
 
     void evaluate_fitness() {
+        // evaluate fitness from
+        sum_fitness = 0;
+        for(auto& p:population) 
+            sum_fitness += (p.eval_fitness(function));
 
-        for(auto& p:population) {
-            auto fit = function(p.first.decode());
-            p.second = fit;
-        }
+        sum_probability = 0;
+        for(auto& p:population)
+            // todo
+            sum_probability += p.eval_probability(sum_fitness, population.size());
     }
 
-    std::tuple<population_t, population_t> selection() {
-        std::sort(population.begin(), population.end(), fitness_compare);
-        return std::make_tuple(population[0], population[1]);
+    //=========================================================
+    // selection algorithm
+    //=========================================================
+    // roulette wheeling selection
+    std::tuple<size_t, size_t> select_with_rouletewheeling() {
+
+        auto select = [&]() {
+            std::uniform_real_distribution<scalar_t> dis(0.0, 1.0);
+            scalar_t random_number = dis(gen);
+
+            size_t i=0;
+            while(random_number>0.0) {
+                random_number -= population[i].probability;
+                i++;
+            }
+            return i;
+        };
+
+        return std::make_tuple(select(), select());
     }
 
-    void crossover(population_t& x, population_t& y) {
-        // std::cout << x.first.to_string() << " " << y.first.to_string() << std::endl;
-        x.first.crossover(y.first, 4);
-        // std::cout << x.first.to_string() << " " << y.first.to_string() << std::endl;
+    //=========================================================
+    // crossover algorithm
+    //=========================================================
+    bool crossover(populate_t& x, populate_t& y) {
+
+        std::uniform_real_distribution<double> prob(0.0, 1.0);
+        if(prob_crossover<prob(gen)) return false;
+
+        std::uniform_int_distribution<size_t> range(0, len-1);
+        size_t r_start = range(gen);
+        size_t r_end   = range(gen);
+
+        x.chromosome.crossover(y.chromosome, r_start, r_end);
+        return true;
     }
 
-    void mutation() {
+    //=========================================================
+    // mutation algorithm
+    //=========================================================
+    bool mutation(populate_t& x) {
         // convetional mutation
+        std::uniform_real_distribution<double> prob(0.0, 1.0);
+        if(prob_mutation<prob(gen)) return false;
 
+        // mutate
+        x.chromosome.mutate();
+        return true;
     }
 
-    void run() {
-
-        for(size_t i=0; i<50; i++) {
-
-            population_t x, y;
-            std::tie(x, y) = selection();
-            // std::cout << x.first.to_string() << " " << y.first.to_string() << std::endl;
-            std::cout << population[0].first.to_string() << " " << population[1].first.to_string() << std::endl;
-            std::cout << population[0].second << " " << population[1].second << std::endl;
-            crossover(x, y);
-            population[0] = x;
-            population[1] = y;
-            evaluate_fitness();
-            // std::cout << x.first.to_string() << " " << y.first.to_string() << std::endl;
-            std::cout << population[0].first.to_string() << " " << population[1].first.to_string() << std::endl;
-            std::cout << population[0].second << " " << population[1].second << std::endl;
-        }
-
+    //=========================================================
+    // replacement algorithm
+    //=========================================================
+    // replace parent
+    void replace_parent(size_t index, const populate_t x) {
+        population[index] = x;
     }
-
-    static inline bool fitness_compare(const population_t& l, const population_t& r) {
-        return l.second < r.second;
-    } 
+    // replace worst: the highest fitness value
+    void replace_worst(const populate_t x) {
+        auto elem = std::max_element(population.begin(), population.end(), [](populate_t a, populate_t b){ return a.fitness<b.fitness; });
+        (*elem) = x;
+    }
 
 protected:
-    std::vector<population_t> population;
-    function_t function;
+    function_t  function;
+    std::vector<populate_t> population;
+    scalar_t    sum_fitness;
+    double      sum_probability;
+    double      prob_crossover;
+    double      prob_mutation;
 };
 
 //// ///////////////////////////////////////////////
--- test/hw7/test.cpp
+++ test/hw7/test.cpp
@@ -9,6 +9,10 @@ auto functions = hw7::construct_functions();
 
 TEST(GENETIC_ALGORITHM, BasicAssertions) {
 
-    auto ga = GeneticAlgorithm(functions[0]);
-    ga.run();
+    auto ga0 = GeneticAlgorithm<8, double, double>(functions[0]);
+    // ga0.run(50);
+
+    auto ga1 = GeneticAlgorithm<16, double, double>(functions[1]);
+    // ga1.run(50);
+    // ga1.print();
 }
